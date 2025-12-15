2fcb9999384333094ab9eaa97b15663dd140114b
BVoermann <Benjamin-Voermann@web.de>
2025-12-15 21:43:57 +0100

    adds systems_layout to include in the various home.html files; adds context for dnd

--- dnd/views.py
+++ dnd/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 
 def home(request):
     """D&D home page."""
-    return render(request, 'dnd/home.html')
+    context = {
+        'title': 'Dungeons & Dragons',
+        'subtitle': "The World's Greatest Roleplaying Game",
+        'welcome': 'Gather your party and venture forth into realms of magic, mystery, and endless adventure. '
+                   'Heroes are forged in the fires of battle and the bonds of friendship.',
+        'quote': '"The story is yours to tell."',
+        'campaign_subtitle': 'Legendary quests await',
+        'sessions_subtitle': 'Tales of valor and glory',
+        'lore_subtitle': 'Ancient knowledge',
+        'characters_subtitle': 'Heroes of legend',
+        'media_subtitle': 'Treasures and artifacts',
+        'search_subtitle': 'Explore the archives',
+    }
+    return render(request, 'dnd/home.html', context=context)
