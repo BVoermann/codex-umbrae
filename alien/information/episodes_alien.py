2dfea0a4b054496e4e39573cd32ea0d674031011
BVoermann <Benjamin-Voermann@web.de>
2026-03-25 10:24:57 +0100

    adds the alien playlist link

--- alien/information/episodes_alien.py
+++ alien/information/episodes_alien.py
@@ -1,5 +1,1 @@
-playlist_SdG = ""
-
-transcripts_first_part_SdG = "https://raw.githubusercontent.com/BVoermann/transcripts/refs/heads/SdG/Session_"
-
-git_tree_SdG = f"https://api.github.com/repos/BVoermann/transcripts/git/trees/SdG?recursive=1"
+playlist_SdG = "https://www.youtube.com/embed/videoseries?list=PL49uYL3GdZvU_Ckw_qi55j27205lUpG_o"
--- alien/views.py
+++ alien/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import requests
 from alien.information.context_alien import context
-from alien.information.episodes_alien import playlist_SdG, transcripts_first_part_SdG, git_tree_SdG
+from alien.information.episodes_alien import playlist_SdG
 from codexumbrae.auth_views import require_sessions_password
 
 def home(request):
@@ -19,24 +19,12 @@ def sessions(request):
     campaign_id = request.GET.get('campaign')
     if campaign_id == 'Streitwagen-der-Goetter':
         playlist_url = playlist_SdG
-        transcript_url = transcripts_first_part_SdG
-        response = requests.get(git_tree_SdG)
     else:
         playlist_url = []
-        transcript_url = ""
-        episode_range = range(1)
-        response = None
-
-    if response:
-        data = response.json()
-        files = [item for item in data['tree'] if item['type'] == 'blob']
-        episode_range = range(1, len(files))
 
     return render(request, 'systems_sessions.html', context={
         **context,
         'playlist_url': playlist_url,
-        'transcript_url': transcript_url,
-        'episode_range': episode_range,
     })
 
 
